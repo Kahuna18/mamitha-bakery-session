9f3f6602e7e916846e0ee35e91de591cb347ac40
kahunaraihan01 <11407@smpn1godean.sch.id>
2026-06-16 21:32:07 +0700

    Fix maps location to Sleman, Yogyakarta on contact page

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -65,8 +65,8 @@
                 </a>
             </div>
             <div>
-                <div class="bg-gray-200 rounded-xl h-80 flex items-center justify-center">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0!2d106.8!3d-6.2!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTInMDAuMCJTIDEwNsKwNDgnMDAuMCJF!5e0!3m2!1sen!2sid!4v1" width="100%" height="100%" style="border:0; border-radius: 12px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="bg-gray-250 rounded-xl h-80 flex items-center justify-center">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.271986185861!2d110.25038067404924!3d-7.760952876966746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7af7e11f6dcb4b%3A0x63158b6cfa3c0255!2sMamitha%20bakery!5e0!3m2!1sen!2sid!4v1781620147486!5m2!1sen!2sid" width="100%" height="100%" style="border:0; border-radius: 12px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
